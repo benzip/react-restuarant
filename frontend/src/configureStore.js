f632ec63f0d3f9857b5868ca34b0fe3d5cb0882d
nattapol sittikijyothin <benz_ace@hotmail.com>
2018-02-16 09:41:57 +0700

    save

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -4,11 +4,11 @@ import promise from "redux-promise";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/rootSaga";
-export default function configureStore(initialState) {
+export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  const middleware = applyMiddleware(sagaMiddleware, promise, logger);
+  const middleware = applyMiddleware(sagaMiddleware, logger);
   const store = createStore(rootReducer, {}, composeEnhancers(middleware));
 
   sagaMiddleware.run(rootSaga);
